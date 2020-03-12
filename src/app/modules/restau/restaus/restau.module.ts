8308b8534cb69afb7e875c0cae66cea1e8f4534d
Amine BOUZIANE <amine.bouziane@niji.fr>
2020-03-12 17:30:23 +0100

    start flexing

--- src/app/modules/restau/restaus/restau.module.ts
+++ src/app/modules/restau/restaus/restau.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RestausComponent } from './restaus.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 
 @NgModule({
   declarations: [RestausComponent],
   imports: [
-    CommonModule
+    CommonModule,SharedModule
   ],
   exports:[RestausComponent]
 })
